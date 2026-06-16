6fe34ae8a02014148c5d9eff75c978cf006c5375
kimnguyen110806-tech <kimnguyen110806@gmail.com>
2026-06-16 14:02:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM TẠI NGÂN HÀNG_ĐỀ TÀI 2_TRẦN HỒNG KIM NGUYÊN")
 
 # Nhập dữ liệu
 C = st.number_input(
